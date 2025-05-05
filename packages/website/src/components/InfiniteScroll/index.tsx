377fef87e5cff5dc4ae2fc5e954d78b3d2ac92fb
Oleh Khmaruk <pelmenstar1@gmail.com>
2025-05-05 20:59:31 +0300

    Fix InfiniteScroll

--- packages/website/src/components/InfiniteScroll/index.tsx
+++ packages/website/src/components/InfiniteScroll/index.tsx
@@ -1,6 +1,14 @@
 'use client';
 
-import { ReactNode, Ref, useEffect, useRef, useState } from 'react';
+import {
+  ReactNode,
+  Ref,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 import { IndeterminateCircularProgress } from '../IndeterminateCircularProgress';
 import { useNotification } from '../Notification';
 
@@ -35,6 +43,7 @@ export function InfiniteScroll<T>({
   children,
 }: InfiniteScrollProps<T>) {
   const maxPageRef = useRef(0);
+  const requestInProgressRef = useRef(false);
 
   const [hasMoreElements, setHasMoreElements] = useState(true);
   const [items, setItems] = useState<T[]>([]);
@@ -43,26 +52,37 @@ export function InfiniteScroll<T>({
 
   const notification = useNotification();
 
+  const requestNextPage = useCallback(() => {
+    if (!requestInProgressRef.current) {
+      requestInProgressRef.current = true;
+
+      requestPage(maxPageRef.current + 1)
+        .then((page) => {
+          if (page.length === 0) {
+            setHasMoreElements(false);
+          } else {
+            setItems((items) => [...items, ...page]);
+          }
+
+          maxPageRef.current++;
+          requestInProgressRef.current = false;
+        })
+        .catch((error: unknown) => {
+          console.error(error);
+
+          notification.show('Не вийшло завантажити сторінку', 'error');
+          requestInProgressRef.current = false;
+        });
+    }
+  }, [notification, requestPage]);
+
   useEffect(() => {
     const { current: maxMarker } = maxMarkerRef;
 
     const observer = new IntersectionObserver(
       () => {
-        requestPage(maxPageRef.current + 1)
-          .then((page) => {
-            if (page.length === 0) {
-              setHasMoreElements(false);
-            } else {
-              setItems((items) => [...items, ...page]);
-            }
-
-            maxPageRef.current++;
-          })
-          .catch((error: unknown) => {
-            console.error(error);
-
-            notification.show('Не вийшло завантажити сторінку', 'error');
-          });
+        console.log('observe');
+        requestNextPage();
       },
       {
         root: null,
@@ -78,7 +98,19 @@ export function InfiniteScroll<T>({
     return () => {
       observer.disconnect();
     };
-  }, [notification, requestPage]);
+  }, [requestNextPage]);
+
+  useLayoutEffect(() => {
+    const { current: maxMarker } = maxMarkerRef;
+
+    if (maxMarker !== null) {
+      const bounds = maxMarker.getBoundingClientRect();
+
+      if (bounds.top <= window.innerHeight) {
+        requestNextPage();
+      }
+    }
+  }, [items, requestNextPage]);
 
   return (
     <div className={classNames(styles.root, className)}>
--- packages/website/src/components/LazyImageScroll/LazyImageScroll.stories.tsx
+++ packages/website/src/components/LazyImageScroll/LazyImageScroll.stories.tsx
@@ -26,20 +26,29 @@ function repeat<T>(item: T, count: number): T[] {
   return result;
 }
 
-function requestPage(page: number) {
-  return Promise.resolve(
-    repeat(
-      page % 2 === 0
-        ? 'https://i.imgur.com/gbt7JG7.jpg'
-        : 'https://i.imgur.com/OEuYkKXl.png',
-      40
-    )
-  );
+function pageRequester(n: number) {
+  return (page: number) => {
+    return Promise.resolve(
+      repeat(
+        page % 2 === 0
+          ? 'https://i.imgur.com/gbt7JG7.jpg'
+          : 'https://i.imgur.com/OEuYkKXl.png',
+        n
+      )
+    );
+  };
 }
 
-export const Primary: Story = {
+export const LongPages: Story = {
   args: {
-    requestPage,
+    requestPage: pageRequester(40),
+    getImageSource: (value) => value,
+  },
+};
+
+export const SmallPages: Story = {
+  args: {
+    requestPage: pageRequester(1),
     getImageSource: (value) => value,
   },
 };
