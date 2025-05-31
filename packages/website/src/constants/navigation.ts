e4ff87e6fe6c8e65a8bd639d3d3de98516ff54ae
Oleh Khmaruk <pelmenstar1@gmail.com>
2025-05-31 13:47:27 +0300

    Add more links to header

--- packages/website/src/constants/navigation.ts
+++ packages/website/src/constants/navigation.ts
@@ -2,4 +2,6 @@ export const navigationMainRoutes: { title: string; href: string }[] = [
   { title: 'Студентство', href: '/students' },
   { title: 'Розклад', href: '/schedule' },
   { title: 'Опитування', href: '/polls' },
+  { title: 'Події', href: '/events' },
+  { title: 'Галерея', href: '/gallery' },
 ];
