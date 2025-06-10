457b0400d5297ef1325198c4fce3ffa314b6ea76
Oleh Khmaruk <pelmenstar1@gmail.com>
2025-06-10 21:49:56 +0300

    Move SVG file icon to inline

--- packages/website/src/components/Footer/index.tsx
+++ packages/website/src/components/Footer/index.tsx
@@ -6,6 +6,14 @@ import styles from './index.module.scss';
 import { classNames } from '@/utils/classNames';
 import { navigationMainRoutes } from '@/constants/navigation';
 
+import logo from '@/images/logo.png';
+import { InstagramIcon } from '@/icons/InstagramIcon';
+import { TelegramIcon } from '@/icons/TelegramIcon';
+import { TikTokIcon } from '@/icons/TikTokIcon';
+import { MailIcon } from '@/icons/MailIcon';
+import { QuestionIcon } from '@/icons/QuestionIcon';
+import { ReactNode } from 'react';
+
 export interface FooterProps {
   className?: string;
 }
@@ -14,6 +22,26 @@ const items: { title: string; href: string }[] = [
   { title: 'Головна', href: '/home' },
   ...navigationMainRoutes,
 ];
+
+type LinkWithIconProps = {
+  to: string;
+  small?: boolean;
+  children: ReactNode;
+};
+
+function LinkWithIcon({ to, small, children }: LinkWithIconProps) {
+  return (
+    <Link
+      to={to}
+      className={classNames(
+        styles['link-with-icon'],
+        small && styles['link-with-icon-small']
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
 
 export function Footer({ className }: FooterProps) {
   return (
@@ -35,16 +63,15 @@ export function Footer({ className }: FooterProps) {
       <div className={styles.section}>
         <Typography as="strong">Підтримка</Typography>
 
-        <div className={styles.linkWithIcon}>
-          <img src="/icons/Question.svg" width={15} height={15} alt="Bot" />
+        <LinkWithIcon to="https://t.me/fpm_sc_bot" small>
+          <QuestionIcon />
+          Чат-бот
+        </LinkWithIcon>
 
-          <Link to="https://t.me/fpm_sc_bot">Чат-бот</Link>
-        </div>
-
-        <div className={styles.linkWithIcon}>
-          <img src="/icons/Logo.svg" width={15} height={15} alt="Bot" />
-          <Link to="/home">Про нас</Link>
-        </div>
+        <LinkWithIcon to="/home">
+          <img src={logo} width={15} height={15} alt="Bot" />
+          Про нас
+        </LinkWithIcon>
       </div>
 
       <div className={styles.section}>
@@ -54,29 +81,19 @@ export function Footer({ className }: FooterProps) {
       </div>
 
       <div className={styles.icons}>
-        <a href="mailto:sr.fam.kpi@gmail.com" aria-label="Email">
-          <img src="/icons/Mail.svg" width={24} height={24} alt="Mail" />
-        </a>
+        <LinkWithIcon to="mailto:sr.fam.kpi@gmail.com">
+          <MailIcon />
+        </LinkWithIcon>
 
-        <Link to="https://www.instagram.com/fam_kpi/">
-          <img
-            src="/icons/Instagram.svg"
-            width={24}
-            height={24}
-            alt="Instagram"
-          />
-        </Link>
-        <Link to="https://t.me/primat_kpi">
-          <img
-            src="/icons/Telegram.svg"
-            width={24}
-            height={24}
-            alt="Telegram"
-          />
-        </Link>
-        <Link to="https://www.tiktok.com/@fam_kpi?_t=ZM-8vrGKJSe9Rt&_r=1">
-          <img src="/icons/TikTok.svg" width={24} height={24} alt="TikTok" />
-        </Link>
+        <LinkWithIcon to="https://www.instagram.com/fam_kpi/">
+          <InstagramIcon />
+        </LinkWithIcon>
+        <LinkWithIcon to="https://t.me/primat_kpi">
+          <TelegramIcon />
+        </LinkWithIcon>
+        <LinkWithIcon to="https://www.tiktok.com/@fam_kpi?_t=ZM-8vrGKJSe9Rt&_r=1">
+          <TikTokIcon />
+        </LinkWithIcon>
       </div>
     </footer>
   );
