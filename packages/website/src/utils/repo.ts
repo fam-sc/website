75e28cbec879e22b52b552c4034c3ba2e943cc5d
Oleh Khmaruk <pelmenstar1@gmail.com>
2025-06-28 17:56:02 +0300

    attempt

--- packages/website/src/utils/repo.ts
+++ packages/website/src/utils/repo.ts
@@ -1,6 +1,6 @@
 import { Repository } from '@data/repo';
 import { AppLoadContext } from 'react-router';
-import { getDatabase } from './apiEnv.node';
+import { getDatabase } from './d1Db';
 
 export function repository(context: AppLoadContext): Repository {
   const db = import.meta.env.DEV ? getDatabase() : context.cloudflare.env.DB;
