481fa5c034799729453fbc83206fcd8ea30141cf
Oleh Khmaruk <pelmenstar1@gmail.com>
2025-07-16 22:01:27 +0300

    Update dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,11 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { loadEnv, PluginOption } from 'vite';
+import { loadEnv } from 'vite';
 
 const env = loadEnv('', './packages/website', '');
 
-const plugins = [react(), tsconfigPaths()] as PluginOption[];
+const plugins = [react(), tsconfigPaths()];
 
 export default defineConfig({
   plugins,
@@ -18,10 +18,10 @@ export default defineConfig({
     projects: [
       {
         plugins,
-        env,
         test: {
           include: ['**/*.test.ts'],
           name: 'unit',
+          env,
           environment: 'node',
         },
       },
