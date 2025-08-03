2aaa9886c69b63c54052e59d93db2c9fa78b45a8
Oleh Khmaruk <pelmenstar1@gmail.com>
2025-08-03 11:41:25 +0300

    Improve lint, prettify yml

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -1,47 +1,18 @@
-import fsp from 'node:fs/promises';
-import path from 'node:path';
-
 import { yarn } from './process';
 
-const BUILD_SCRIPT = 'build:no-emit';
 const STYLELINT_PATTERN = `**/*.{css,scss}`;
 
 // Only check files that ESLint prettier plugin doesn't check.
-const PRETTIER_PATTERN = '**.{json,css,scss,md}';
+const PRETTIER_PATTERN = '**.{json,css,scss,md,yml}';
 
-async function hasScript(filePath: string, name: string): Promise<boolean> {
-  type Package = {
-    scripts?: Record<string, string>;
-  };
-
-  const content = await fsp.readFile(filePath, 'utf8');
-  const packageInfo = JSON.parse(content) as Package;
-
-  return packageInfo.scripts?.[name] !== undefined;
-}
-
-async function buildAllPackages() {
-  const packagesDir = path.join(import.meta.dirname, '../packages');
-  const packageNames = await fsp.readdir(packagesDir);
-
-  await Promise.all(
-    packageNames.map(async (name) => {
-      const cwd = path.join(packagesDir, name);
-
-      try {
-        if (await hasScript(path.join(cwd, 'package.json'), BUILD_SCRIPT)) {
-          await phase(`Build '${name}'`, [BUILD_SCRIPT], cwd);
-        }
-      } catch (error: unknown) {
-        throw new Error(`Build failed on '${name}' package`, { cause: error });
-      }
-    })
-  );
-}
-
-async function phase(name: string, args: string[], cwd?: string) {
-  await yarn(args, cwd);
-  console.log(`> ${name} finished`);
+async function phase(name: string, args: string[]) {
+  try {
+    await yarn(args);
+    console.log(`> ${name} finished`);
+  } catch (error: unknown) {
+    console.error(`> ${name}`);
+    console.error(error);
+  }
 }
 
 async function main() {
@@ -69,17 +40,21 @@ async function main() {
       PRETTIER_PATTERN,
     ]);
   } else {
-    await Promise.all([
-      phase('eslint', [
-        'eslint',
-        '--cache',
-        '--cache-location',
-        '.eslintcache',
-      ]),
-      phase('stylelint', ['stylelint', '--cache', STYLELINT_PATTERN]),
-      phase('prettier', ['prettier', '--cache', '--check', PRETTIER_PATTERN]),
-      buildAllPackages(),
-    ]);
+    try {
+      await Promise.allSettled([
+        phase('eslint', [
+          'eslint',
+          '--cache',
+          '--cache-location',
+          '.eslintcache',
+        ]),
+        phase('stylelint', ['stylelint', '--cache', STYLELINT_PATTERN]),
+        phase('prettier', ['prettier', '--cache', '--check', PRETTIER_PATTERN]),
+        phase('build', ['tsc', '--build', 'tsconfig.ref.json']),
+      ]);
+    } catch {
+      // Messages about errors are alrady in the terminal
+    }
   }
 }
 
