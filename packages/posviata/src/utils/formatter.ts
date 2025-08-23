3b384a15c7bd0122d9e0f3d47171d15df2a9e718
Oleh Khmaruk <pelmenstar1@gmail.com>
2025-08-23 20:03:17 +0300

    Delete dead code

--- packages/posviata/src/utils/formatter.ts
+++
@@ -1,7 +0,0 @@
-import { formatTwoDigit } from '@sc-fam/shared/string';
-
-import { DateInterval } from '@/hooks/useCountdown';
-
-export function formatCountdown(value: DateInterval): string {
-  return `${formatTwoDigit(value.days)}:${formatTwoDigit(value.hours)}:${formatTwoDigit(value.minutes)}`;
-}
--- packages/posviata/src/utils/math.ts
+++ packages/posviata/src/utils/math.ts
@@ -2,15 +2,3 @@ export type Point = {
   x: number;
   y: number;
 };
-
-export function distanceSquared(
-  x1: number,
-  y1: number,
-  x2: number,
-  y2: number
-) {
-  const dx = x1 - x2;
-  const dy = y1 - y2;
-
-  return dx * dx + dy * dy;
-}
